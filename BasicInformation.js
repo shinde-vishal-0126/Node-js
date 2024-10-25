f3cfd78590b8a1d4ce26e3ab00c499f441628ef2
akshataGit <sakshata212l@gmail.com>
2024-10-26 02:02:33 +0530

    add express js

--- BasicInformation.js
+++ BasicInformation.js
@@ -5,8 +5,9 @@
 // 1. res.end() : used to signal the end of the response in an HTTP server (If you don’t need to send any additional data and just want to signal the end of the response,)
 // ends the response and closes the connection.
 // 2. res.write() : send parts of the response, ( used to send chunks of the response body to the client while the connection is still open.)
-// 3. res.writeHead(200,{ cotentType: 'text/plain'}) :Sends a response header to the request.
+// 3. res.writeHead(200,{ contentType: 'text/plain'}) :Sends a response header to the request.
 // The status code is a 3-digit HTTP status code, like 404. The last argument, headers, are the response headers. Optionally one can give a human-readable statusMessage as the second argument.
+
 
 
 
@@ -25,7 +26,7 @@
 // ==========================================================================================
 // What is api ?
 // ANS: api is services where we have request to data
-// api stands for application programming interface wich communicate with each other
+// api stands for application programming interface which communicate with each other
 
 
 
@@ -38,10 +39,12 @@
 // Express js
 // 1. if you have used express js you no need to used http module
 // also you don't need to used http.createServer() method
-// here you have to used directely app.get("/", (req, res)=>{})
+// here you have to used directly app.get("/", (req, res)=>{})
+// res.status().send() : used to send status code:
 
 // info about express js method 
-// insed of write or end method in express js having send() method to send respone or response body
+// instead of write or end method in express js having send() method to send response or response body
+
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Node-js
-Hello to node js/
+
+
+    - BASIC INFORMATION: 
+    - CORE MODULE:
+    - REPL :
+    - BUFFER:
+    - STREAM : PIPE():
+    - ROUTE:
+    - ASYNCHRONOUS-SYNCHRONOUS:
+    - FS-MODULE:
+    - OS-MODULE:
+    - OWN CUSTOM MODULE:
+    - PATH MODULE:
+    - WEB-SERVER:
+    - JSON:
+    - USER API EXAMPLE:
+    - NPM:
+    - NODE WRAPPER FUNCTION:
+    - EVENT MODULE: 
+    - SENT MAIL:
+
+    - EXPRESS JS:
+    - ROUTE IN EXPRESS JS:
+    - SERVE STATIC FILE IN EXPRESS JS:
+    
