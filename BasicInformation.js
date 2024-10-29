3cc783ba1a3e1bbd35e066c95003729463d807fd
akshataGit <sakshata212l@gmail.com>
2024-10-29 12:15:42 +0530

    feat: add template-engine

--- BasicInformation.js
+++ BasicInformation.js
@@ -54,7 +54,7 @@
 
 
 
-// to create package.json file used npm init -y (-y falg used get by default value and create the package.json)
+// to create package.json file used npm init -y (-y flag used get by default value and create the package.json)
 
 
 
@@ -65,3 +65,61 @@
 // put:
 // patch :
 // delete:
+
+// ===============================================================================================
+
+// node js run top to bottom 
+// ex if you have defined the same route that situation first route is render then send single to end of the response.
+
+
+
+// Template engine 
+// 1.first we need to set the views as default folder where you have to defined the template file are located set in above way app.set('views', 'path of the view folder')
+// 2. second we need to set template engine which you have to used  in above way app.set('view engine', 'template engine name')
+// 3. you have to used render method to render the html or template engine file so render method having the  2 parameter first on in file name and 2nd one is object to pass the dynamic content 
+
+// Q // In express js is possible to change the directory name views to any other name like view to template 
+// yes it is possible in following way 
+const templatePath = path.join(__dirname, 'template');
+app.set('views', templatePath)
+
+
+// ===============================================================================================
+
+
+// what is partials in express js
+
+// 1. In Express.js, "partials" refer to reusable view templates (often HTML or embedded JavaScript) that represent smaller parts or sections of a web page.
+// 2. They are particularly useful in template engines like EJS (Embedded JavaScript) or Handlebars, where you want to break down a larger page into smaller, manageable, and reusable components.
+
+// Create partials: Let's say you have a common header and footer for your website, which you want to reuse across multiple pages.
+
+// 1. Create a partials folder inside your views directory.
+// 2.Add header.hbs and footer.hbs files in the partials folder.
+// 3. next we have to register the partial with give template engine in following way 
+// first you have to import template engine you have to used in your application
+const hbs = require('hbs');
+const partialsPath = path.join(__dirname,'partical')
+console.log(partialsPath)
+console.log(viewPath);
+
+const app = express();
+app.set('views',viewPath )
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(partialsPath);
+
+// 4 then in template file you have to defined the partial template file using following syntax 
+{/* <body>
+    {{>header}}
+    <h1>Finally dynamic content are here .... {{name}}</h1>
+</body> */}
+
+
+// Benefits of Partials
+// Reusability: You can create once and reuse in multiple places.
+// Maintainability: Changes to common sections (like a header or footer) are made in one file and apply everywhere.
+// Consistency: Ensures consistent layouts across all pages.
+
+// ===============================================================================================
--- ThirdPartyPackages.js
+++ ThirdPartyPackages.js
@@ -4,4 +4,10 @@
 // 2. validator : used for string validation and sanitizers
 // 3. nodemon : used to application by automatically restarting the node application when file changes in the directory are detected.
 // 4. nodemailer : this package used to send mail through node js  // https://nodemailer.com/
-// 5. (to used fack SMPT (simple mail transfer protocol) server you have to use ethereal)  // https://ethereal.email/create
+// 5. (to used feck SMTP (simple mail transfer protocol) server you have to use ethereal)  // https://ethereal.email/create
+
+
+// used Template engine package
+// hbs: Handlebar
+// pug:
+// ejs:
